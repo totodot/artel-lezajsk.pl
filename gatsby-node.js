616bfdc930a541b2599d993261a0773e5243df83
totodot <toto.zetka@gmail.com>
2019-06-03 20:20:41 +0200

    Add full fill google maps

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,7 +5,7 @@
  */
 
 // You can delete this file if you're not using
-const path = require("path")
+const path = require('path');
 
 exports.createPages = async ({ actions: { createPage }, graphql }) => {
   const products = await graphql(`
@@ -18,14 +18,11 @@ exports.createPages = async ({ actions: { createPage }, graphql }) => {
         }
       }
     }
-  `)
+  `);
 
   const blog = await graphql(`
     {
-      allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___date] }
-        limit: 1000
-      ) {
+      allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }, limit: 1000) {
         edges {
           node {
             frontmatter {
@@ -36,43 +33,41 @@ exports.createPages = async ({ actions: { createPage }, graphql }) => {
         }
       }
     }
-  `)
+  `);
 
-  products.data.allProductsJson.edges.forEach(edge => {
-    const product = edge.node
+  products.data.allProductsJson.edges.forEach((edge) => {
+    const product = edge.node;
 
     createPage({
       path: `/gql/${product.slug}/`,
-      component: require.resolve("./src/templates/product-graphql.js"),
+      component: require.resolve('./src/templates/product-graphql.js'),
       context: {
         slug: product.slug,
       },
-    })
-  })
+    });
+  });
   blog.data.allMarkdownRemark.edges.forEach(({ node }) => {
-    console.log('dsadasd')
-    console.log(node);
     createPage({
       path: `/posts/${node.frontmatter.id}/`,
-      component: require.resolve("./src/templates/blog-template.js"),
+      component: require.resolve('./src/templates/blog-template.js'),
       context: {
         id: node.frontmatter.id,
       }, // additional data can be passed via context
-    })
-  })
-}
+    });
+  });
+};
 
 exports.onCreateNode = ({ node, actions }) => {
-  const { createNodeField } = actions
-  const { frontmatter } = node
+  const { createNodeField } = actions;
+  const { frontmatter } = node;
   if (frontmatter) {
-    const { thumbnail } = frontmatter
+    const { thumbnail } = frontmatter;
     if (thumbnail) {
-      if (thumbnail.indexOf("/assets") === 0) {
+      if (thumbnail.indexOf('/assets') === 0) {
         frontmatter.thumbnail = path.relative(
           path.dirname(node.fileAbsolutePath),
-          path.join(__dirname, "/static/", thumbnail)
-        )
+          path.join(__dirname, '/static/', thumbnail),
+        );
       }
     }
   }
@@ -85,4 +80,4 @@ exports.onCreateNode = ({ node, actions }) => {
   //     value,
   //   })
   // }
-}
+};
--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -8,20 +8,20 @@ const Footer = () => (
       <div className="row">
         <div className="col-12 col-md-6">
           <h1>elo</h1>
-          
+
           <h1>elo</h1>
           <h1>elo</h1>
           <h1>elo</h1>
           <h1>elo</h1>
           <h1>elo</h1>
         </div>
-        <div className="col-12 col-md-6 p-none">
+        <div className="col-12 col-md-6 p-none footer__map-col">
           <Map
             isMarkerShown
             googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
             loadingElement={<div style={{ height: '100%' }} />}
-            containerElement={<div className="footer__map" />}
-            mapElement={<div style={{ height: '100%' }} />}
+            containerElement={<div className="footer__map-container" />}
+            mapElement={<div className="footer__map-element" />}
           />
         </div>
       </div>
