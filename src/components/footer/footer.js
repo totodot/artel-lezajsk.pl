530235ef5a66d787005cfe48b91eff77723e867a
totodot <toto.zetka@gmail.com>
2019-06-05 20:24:22 +0200

    Add footer contact info

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,19 +1,52 @@
 import React from 'react';
 import cx from 'classnames';
 import Map from './googleMap';
+import PhoneSvg from '../../images/icons/phone.inline.svg';
+import EnvelopeSvg from '../../images/icons/envelope.inline.svg';
 
 const Footer = () => (
   <footer className="footer">
     <div className="container">
       <div className="row">
         <div className="col-12 col-md-6">
-          <h1>elo</h1>
-
-          <h1>elo</h1>
-          <h1>elo</h1>
-          <h1>elo</h1>
-          <h1>elo</h1>
-          <h1>elo</h1>
+          <div className="footer__info-container">
+            <div className="footer__company-name">Artel</div>
+            <div className="footer__company-subname">artykuły elektryczne</div>
+            <div className="footer__address">
+              <div>Żwirki i Wigury 9</div>
+              <div>37-300 Leżajsk</div>
+            </div>
+            <div className="footer__contact footer-contact">
+              <a href="tel:+48 17 242 79 45" className="footer-contact__item">
+                <span className="footer-contact__icon">
+                  <PhoneSvg />
+                </span>
+                17 242 79 45
+              </a>
+              <a href="mailto:artel@biuro.pl" className="footer-contact__item">
+                <span className="footer-contact__icon">
+                  <EnvelopeSvg />
+                </span>
+                {/* TODO change email */}
+                artel@biuro.pl
+              </a>
+            </div>
+            <div className="working-hours">
+              <div className="working-hours__header">Godziny pracy</div>
+              <div className="working-hours__item">
+                <div className="working-hours__days">pon.-pt.:</div>
+                <div className="working-hours__hours">08:00-17:00</div>
+              </div>
+              <div className="working-hours__item">
+                <div className="working-hours__days">sob.:</div>
+                <div className="working-hours__hours">08:00-13:00</div>
+              </div>
+              <div className="working-hours__item">
+                <div className="working-hours__days">niedz.:</div>
+                <div className="working-hours__hours">Zamknięte</div>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="col-12 col-md-6 p-none footer__map-col">
           <Map
@@ -29,3 +62,7 @@ const Footer = () => (
   </footer>
 );
 export default Footer;
+
+// TODO;
+// underline current day (working hours)
+// add info about holidays
--- src/components/header.js
+++ src/components/header.js
@@ -48,7 +48,7 @@ const Header = ({ siteTitle }) => {
               <nav className="nav">
                 <ul className="nav__list">
                   {menuLinks.map(({ name, link }) => (
-                    <li className="nav__item">
+                    <li className="nav__item" key={link}>
                       <Link to={link} className="nav__link" activeClassName="nav__link_active">
                         {name}
                       </Link>
@@ -63,7 +63,7 @@ const Header = ({ siteTitle }) => {
       <nav className={cx('mobile-nav', { 'mobile-nav_active': isHamburgerOpen })}>
         <ul className="mobile-nav__list">
           {menuLinks.map(({ name, link }) => (
-            <li className="mobile-nav__item">
+            <li className="mobile-nav__item" key={link}>
               <Link to={link} className="mobile-nav__link" activeClassName="nav__link_active">
                 {name}
               </Link>
