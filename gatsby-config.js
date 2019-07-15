70acab728d8692524a85cee052af858a506e5594
totodot <toto.zetka@gmail.com>
2019-07-15 10:27:04 +0200

    Remove yarn.lock

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,13 +80,15 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Sklep Elektryczny Artel - Leżajsk',
+        short_name: 'Artel',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#1b4fd2',
+        theme_color: '#1b4fd2',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        // TODO: change fav icon
+        icon: 'src/images/logo-mark.svg',
+        crossOrigin: 'use-credentials',
       },
     },
     {
@@ -99,6 +101,7 @@ module.exports = {
         manualInit: true,
       },
     },
+
     // 'gatsby-plugin-netlify-identity-widget',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
