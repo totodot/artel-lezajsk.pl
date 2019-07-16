c04865551e8c01622b2011e8328c076af07ea2a0
totodot <toto.zetka@gmail.com>
2019-07-16 21:43:50 +0200

    Change fav-icon extension

--- siteConfig.js
+++ siteConfig.js
@@ -33,7 +33,7 @@ const ada = {
   description:
     'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
   color: '#c43838',
-  icon: 'src/images/ada-fav.svg',
+  icon: 'src/images/ada-fav.png',
   street: 'Kasprowicza 2',
   postCode: '37-100',
   city: 'Łańcut',
